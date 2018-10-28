188aecc1997e848d7995f5b2a1604a291cd41570
biu <hezhou920@gmail.com>
2018-10-28 20:55:42 +0800

    增加数据层实现，使页面可以正常访问

--- spittr/src/main/java/spittr/config/RootConfig.java
+++ spittr/src/main/java/spittr/config/RootConfig.java
@@ -1,16 +1,26 @@
 package spittr.config;
 
+import java.util.regex.Pattern;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
+
+import spittr.config.RootConfig.WebPackage;
 
 @Configuration
+@Import(DataConfig.class)
 @ComponentScan(basePackages = {"spittr"},
         excludeFilters = {
-                @Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
+                @Filter(type = FilterType.CUSTOM, value = WebPackage.class)
         })
 public class RootConfig {
-
+    public static class WebPackage extends RegexPatternTypeFilter {
+        public WebPackage() {
+            super(Pattern.compile("spittr\\.web"));
+        }
+    }
 }
--- spittr/src/main/java/spittr/data/SpittleRepository.java
+++ spittr/src/main/java/spittr/data/SpittleRepository.java
@@ -1,11 +1,17 @@
 package spittr.data;
 
-import spittr.Spittle;
-
 import java.util.List;
 
+import spittr.Spittle;
+
 public interface SpittleRepository {
+
+    List<Spittle> findRecentSpittles();
+
     List<Spittle> findSpittles(long max, int count);
 
     Spittle findOne(long id);
+
+    void save(Spittle spittle);
+
 }
